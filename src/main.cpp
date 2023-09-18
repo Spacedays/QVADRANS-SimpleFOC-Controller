dc9939733f96bda7e1af77ac76d6e454db84eab7
Spacedays <44454593+Spacedays@users.noreply.github.com>
2023-09-18 09:22:21 -0500

    Replaces custom variant with generic board definition & adds working I2C & uart test

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,48 @@
 #include <Arduino.h>
+#include <SimpleFOC.h>
+
+// HardwareSerial Serial1(PB7, PB6);  // uart1
+HardwareSerial Serial3(PB11, PB10);   // uart3
+MagneticSensorI2C sensor = MagneticSensorI2C(AS5600_I2C);
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(PB10, OUTPUT);  // set pinmodes (uart2)
-  pinMode(PB11, INPUT);
+
+  Serial3.begin(115200);
+  delay(500);
+  Serial3.println("Starting sensor init");
+  delay(500);
   
+  Serial3.println("Wire setup");
+  Wire = TwoWire(PB9, PA15);
+  delay(500);
   
-  Serial.begin(115200); // uart2
+  Serial3.println("Begin");
+  Wire.begin();
+  delay(500);
+  
+  Serial3.println("setck");
+  Wire.setClock(400000);  // STM arduino platform setClock assumes Wire is initialized
+  sensor.init();
 }
 
-String str;
 void loop() {
+  
+  // Blink to visibly show hangs
   digitalWrite(LED_BUILTIN, HIGH);  // PC11
-  Serial.println("High");
-  delay(5000);
+  Serial3.println("High");
+  
+  // i2C test - monitor sensor value
+  sensor.update();
+  Serial3.println(sensor.getAngle());
+  delay(1000);
+  
   digitalWrite(LED_BUILTIN, LOW);
-  Serial.println("Low");
-  delay(5000);
-  if (Serial.available()){
-    Serial.println(Serial.readString());
+  Serial3.println("Low");
+  delay(1000);
+
+  // RX test - echo
+  if (Serial3.available()){
+    Serial3.println(Serial3.readString()); 
   }
 }
